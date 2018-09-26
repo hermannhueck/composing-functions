90dd425309f0c55ec45c9b0e2f4b52c56202405d
Hermann Hueck <hermann.hueck@mailbox.org>
2018-09-26 14:42:37 +0200

    Added impl for F#'s pipe operator |>

---
+++ src/main/scala/playground/PipeApp.scala
@@ -0,0 +1,59 @@
+package playground
+
+object Pipe {
+
+  implicit class PipeForA[A](a: A) {
+    def pipe[B](f: A => B): B = f(a)
+    def |>[B](f: A => B): B = f(a) // F#'s |> operator
+  }
+}
+
+object PipeApp extends App {
+
+  println("\n===== Piping like F#")
+
+  import Pipe._
+  
+
+  val squared: Int => Int = x => x * x
+
+  val res0 = 5 |> squared
+
+  println(res0)
+
+
+  println("-----")
+
+  val s2i: String => Int = _.toInt
+  val plus2: Int => Int = _ + 2
+  val div10By: Int => Double = 10.0 / _
+  val d2s: Double => String = _.toString + " !!!"
+
+
+  // Using regular Scala function invocation
+  val res1 = d2s(div10By(plus2(s2i("3")))) // 2.0 !!!
+  println(res1)
+
+
+  // Using pipe
+  val res2a = "3".pipe(s2i).pipe(plus2).pipe(div10By).pipe(d2s) // 2.0 !!!
+  println(res2a)
+
+  val res2b = "3" pipe s2i pipe plus2 pipe div10By pipe d2s // 2.0 !!!
+  println(res2b)
+
+  // Using F#'s pipe idiom
+  val res2c = "3" |> s2i |> plus2 |> div10By |> d2s // 2.0 !!!
+  println(res2c)
+
+
+  // Using Function1#andThen
+  val res3 = (s2i andThen plus2 andThen div10By andThen d2s)("3") // 2.0 !!!
+  println(res3)
+
+  // Using Function1#andThen
+  val res4 = (d2s compose div10By compose plus2 compose s2i)("3") // 2.0 !!!
+  println(res4)
+
+  println("-----\n")
+}
